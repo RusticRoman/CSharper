64ab73de7ab01145aae0926e4ed84641d5ce3733
RusticRoman <roman.kagan@rust-lang.co>
2024-04-22 09:54:23 -0700

    added source code.

---
+++ ConsoleApp2/BinaryTreeDSF/CountGoodNodesinBinaryTree.cs
@@ -0,0 +1,20 @@
+namespace ConsoleApp2.BinaryTreeDSF;
+
+public class CountGoodNodesinBinaryTree
+{
+    int count = 0;
+    public int GoodNodes(TreeNode root) {
+        CountGoodNodes(root, Int32.MinValue);
+        return count;
+    }
+
+    public void CountGoodNodes(TreeNode? node, int maxVal) {
+        if(node == null) return;
+        if(node.val >= maxVal){
+            count++;
+            maxVal = node.val;
+        }
+        CountGoodNodes(node.left, maxVal);
+        CountGoodNodes(node.right, maxVal);
+    }
+}
