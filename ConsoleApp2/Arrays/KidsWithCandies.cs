a5e093c3598c8f2a18d3c041bc18f8a901022780
RusticRoman <roman.kagan@rust-lang.co>
2024-04-24 21:21:32 -0700

    added source code.

--- ConsoleApp2/Arrays/KidsWithCandies.cs
+++ ConsoleApp2/Arrays/KidsWithCandies.cs
@@ -2,5 +2,20 @@ namespace ConsoleApp2;
 
 public class KidsWithCandies
 {
-    
+    public IList<bool> KidsWithCandiesCalc(int[] candies, int extraCandies) {
+        //find max potential candies in the whole array
+        int maxCandies = 0;
+        foreach (int candiesPerChild in candies){
+            if ((candiesPerChild) > maxCandies)
+            {
+                maxCandies = candiesPerChild;
+            }
+        }
+        var result = new List<bool>();
+        for (int i = 0; i < candies.Length; i++)
+        {
+            result.Add((candies[i] + extraCandies) >= maxCandies);
+        }
+        return result;
+    }
 }
