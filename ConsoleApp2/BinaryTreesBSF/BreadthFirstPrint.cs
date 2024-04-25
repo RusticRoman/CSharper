e56af9082066a98fb6b082fb4f3d94b9704cf61d
RusticRoman <roman.kagan@rust-lang.co>
2024-04-24 21:21:49 -0700

    added source code.

--- ConsoleApp2/BinaryTreesBSF/BreadthFirstPrint.cs
+++ ConsoleApp2/BinaryTreesBSF/BreadthFirstPrint.cs
@@ -2,5 +2,22 @@ namespace ConsoleApp2.BinaryTreeBSF;
 
 public class BreadthFirstPrint
 {
-    
+    public BreadthFirstPrint(TreeNode node)
+    {
+        Queue<TreeNode> nodes = new Queue<TreeNode>();
+        nodes.Enqueue(node);
+        while (nodes.Count > 0)
+        {
+            TreeNode n = nodes.Dequeue();
+            Console.WriteLine(n.val);
+            if (n.left != null)
+            {
+                nodes.Enqueue(n.left);
+            }
+            if (n.right != null)
+            {
+                nodes.Enqueue(n.right);
+            }
+        }
+    }
 }
