444e715700b8b5d187e5f6b27b2410d0fa63e064
RusticRoman <roman.kagan@rust-lang.co>
2024-04-24 21:22:14 -0700

    added source code.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ConsoleApp2.BinaryTreeBSF;
 
 namespace ConsoleApp2;
 
@@ -7,5 +8,14 @@ public class Program1
     public static void Main()
     {
         Console.WriteLine("Hello, World!");
+        TreeNode left2 = new TreeNode(4);
+        TreeNode right2 = new TreeNode(5);
+        TreeNode left3 = new TreeNode(6);
+        TreeNode right3 = new TreeNode(7);
+        TreeNode left1 = new TreeNode(2, left2, right2);
+        TreeNode right1 = new TreeNode(3, left3, right3);
+        TreeNode root = new TreeNode(1, left1, right1);
+        
+        ConsoleApp2.BinaryTreeBSF.BreadthFirstPrint b = new ConsoleApp2.BinaryTreeBSF.BreadthFirstPrint(root);
     }
 }
