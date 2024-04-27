4fd61eac932f894af8bf50f4b1625739aa898985
RusticRoman <roman.kagan@rust-lang.co>
2024-04-26 18:27:43 -0700

    added source code.

--- ConsoleApp2/DataStructures/TreeNode.cs
+++ ConsoleApp2/DataStructures/TreeNode.cs
@@ -1,12 +1,15 @@
 namespace ConsoleApp2.DataStructures;
 
- public class TreeNode {
-     public int val;
-     public TreeNode? left;
-     public TreeNode? right;
-     public TreeNode(int val=0, TreeNode? left=null, TreeNode? right=null) {
-             this.val = val;
-             this.left = left;
-             this.right = right;
-         }
- }
+public class TreeNode
+{
+    public TreeNode? left;
+    public TreeNode? right;
+    public int val;
+
+    public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
