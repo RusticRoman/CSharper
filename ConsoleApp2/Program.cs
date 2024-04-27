58b50b1f0f0491adc9f334f0f54d1dd311b3cd31
RusticRoman <roman.kagan@rust-lang.co>
2024-04-26 18:28:26 -0700

    added source code.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using ConsoleApp2.DataStructures;
+﻿using ConsoleApp2.DataStructures;
 
 namespace ConsoleApp2;
 
@@ -8,14 +7,14 @@ public class Program1
     public static void Main()
     {
         Console.WriteLine("Hello, World!");
-        TreeNode left2 = new TreeNode(4);
-        TreeNode right2 = new TreeNode(5);
-        TreeNode left3 = new TreeNode(6);
-        TreeNode right3 = new TreeNode(7);
-        TreeNode left1 = new TreeNode(2, left2, right2);
-        TreeNode right1 = new TreeNode(3, left3, right3);
-        TreeNode root = new TreeNode(1, left1, right1);
-        
+        var left2 = new TreeNode(4);
+        var right2 = new TreeNode(5);
+        var left3 = new TreeNode(6);
+        var right3 = new TreeNode(7);
+        var left1 = new TreeNode(2, left2, right2);
+        var right1 = new TreeNode(3, left3, right3);
+        var root = new TreeNode(1, left1, right1);
+
         var b = new BreadthFirstPrint(root);
     }
 }
