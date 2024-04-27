dfdcdc951a5ba89bc15b0a72cedb900f314df956
RusticRoman <roman.kagan@rust-lang.co>
2024-04-26 18:27:22 -0700

    added source code.

--- ConsoleApp2/BinaryTreesBSF/LongestZigZag.cs
+++ ConsoleApp2/BinaryTreesBSF/LongestZigZag.cs
@@ -5,36 +5,34 @@ public class LongestZigZag
     public int LongestZigZagSolution(TreeNode? root)
     {
         if (root == null) return 0;
-        
-        int a = maxZigZag(root.left, false, 1);
-        
-        int b = maxZigZag(root.right, true, 1);
-        
+
+        var a = maxZigZag(root.left, false, 1);
+
+        var b = maxZigZag(root.right, true, 1);
+
         return Math.Max(a, b);
     }
 
     public int maxZigZag(TreeNode? node, bool directionRight, int length)
     {
-        if (node == null) return length-1;
-        
+        if (node == null) return length - 1;
+
         int leftLength, rightLength;
-        
+
         // Handling the left child
-        if (directionRight) {
+        if (directionRight)
             // If the previous direction was to the right, now go left
             leftLength = maxZigZag(node.left, false, length + 1);
-        } else {
+        else
             // If the previous direction was to the left, continue from 1 (reset)
             leftLength = maxZigZag(node.left, false, 1);
-        }
         // Handling the right child
-        if (!directionRight) {
+        if (!directionRight)
             // If the previous direction was to the left, now go right
             rightLength = maxZigZag(node.right, true, length + 1);
-        } else {
+        else
             // If the previous direction was to the right, continue from 1 (reset)
             rightLength = maxZigZag(node.right, true, 1);
-        }
         return Math.Max(leftLength, rightLength);
     }
 }
