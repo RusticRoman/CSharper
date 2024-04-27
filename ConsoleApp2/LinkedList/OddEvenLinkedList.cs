0e67cbfba0efb0bf581096a3baf1836a54b49e07
RusticRoman <roman.kagan@rust-lang.co>
2024-04-26 18:28:14 -0700

    added source code.

--- ConsoleApp2/LinkedList/OddEvenLinkedList.cs
+++ ConsoleApp2/LinkedList/OddEvenLinkedList.cs
@@ -1,24 +1,24 @@
 namespace ConsoleApp2;
 
 /**
- *
- *
-    328. Odd Even Linked List (Medium)
-
-    Given the head of a singly linked list, group all the nodes with odd indices together
-    followed by the nodes with even indices, and return the reordered list.
-    The first node is considered odd, and the second node is even, and so on.
-    Note that the relative order inside both the even and odd groups should remain as it was in the input.
-    You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+ * *
+ * *
+ * 328. Odd Even Linked List (Medium)
+ * 
+ * Given the head of a singly linked list, group all the nodes with odd indices together
+ * followed by the nodes with even indices, and return the reordered list.
+ * The first node is considered odd, and the second node is even, and so on.
+ * Note that the relative order inside both the even and odd groups should remain as it was in the input.
+ * You must solve the problem in O(1) extra space complexity and O(n) time complexity.
  */
 public class OddEvenLinkedList
 {
     public ListNode? OddEvenList(ListNode? head)
     {
         if (head == null || head.next == null) return head;
-        ListNode odd = head;
-        ListNode even = head.next;
-        ListNode evenHead = even;
+        var odd = head;
+        var even = head.next;
+        var evenHead = even;
 
         while (even != null && even.next != null)
         {
