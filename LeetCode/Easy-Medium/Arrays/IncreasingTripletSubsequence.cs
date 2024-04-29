75fcaad79899f3149f8896e4e60fea2978398afd
RusticRoman <roman.kagan@rust-lang.co>
2024-04-28 19:15:49 -0700

    added source code.

--- LeetCode/Easy-Medium/Arrays/IncreasingTripletSubsequence.cs
+++ LeetCode/Easy-Medium/Arrays/IncreasingTripletSubsequence.cs
@@ -1,6 +1,79 @@
 namespace ConsoleApp2;
 
+/**
+ 334. Increasing Triplet Subsequence
+
+Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
+ 334. Increasing Triplet Subsequence
+Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
+Example 1:
+Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: Any triplet where i < j < k is valid.
+
+Example 2:
+Input: nums = [5,4,3,2,1]
+Output: false
+Explanation: No triplet exists.
+
+Example 3:
+Input: nums = [2,1,5,0,4,6]
+Output: true
+Explanation: The triplet (3, 4, 5) is valid because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
+
+Constraints:
+
+1 <= nums.length <= 5 * 105
+-231 <= nums[i] <= 231 - 1
+
+Follow up: Could you implement a solution that runs in O(n) time complexity and O(1) space complexity?
+
+Example 1:
+Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: Any triplet where i < j < k is valid.
+
+Example 2:
+Input: nums = [5,4,3,2,1]
+Output: false
+Explanation: No triplet exists.
+
+Example 3:
+Input: nums = [2,1,5,0,4,6]
+Output: true
+Explanation: The triplet (3, 4, 5) is valid because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
+
+Constraints:
+
+1 <= nums.length <= 5 * 105
+-231 <= nums[i] <= 231 - 1
+
+Follow up: Could you implement a solution that runs in O(n) time complexity and O(1) space complexity?
+
+ */
 public class IncreasingTripletSubsequence
 {
-    
+    public bool IncreasingTriplet(int[] nums)
+    {
+        if (nums.Length < 3) return false;
+        int i = Int32.MaxValue, j = Int32.MaxValue;
+        for (int index = 0; index < nums.Length; index++)
+        {
+            if (nums[index] <= i)
+            {
+                i = nums[index];
+            }
+            else if (nums[index] <= j)
+            {
+                j = nums[index];
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
